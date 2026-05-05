0c3588f171873ce4dcb76feb2d59c527bc3550ee
W-SDSS <wakanai027@deltalearns.ca>
2026-05-05 09:27:15 -0700

    w

--- soundboard.py
+++ soundboard.py
@@ -4,34 +4,47 @@ from tkinter import ttk
 import playsound as p
 
 window = tk.Tk()
-window.title("soundboard")
-window.geometry("350x300")
+window.title("SoundBoard")
+window.geometry("350x280")
 window.attributes("-topmost", True)
 
 def dog():
     p.playsound("dog.mp3")
+def cat():
+    p.playsound("cat.mp3")
+def chicken():
+    p.playsound("chicken.mp3")
+def horse():
+    p.playsound("horse.mp3")
+def pig():
+    p.playsound("pig.mp3")
+def cow():
+    p.playsound("cow.mp3")
 
-#
+
+#visu
 dog = tk.PhotoImage(file="dog.png").subsample(5,5)
 cat = tk.PhotoImage(file="cat.png").subsample(5,5)
 chicken = tk.PhotoImage(file="chicken.png").subsample(5,5)
-hourse = tk.PhotoImage(file="hourse.png").subsample(5,5)
-pig = tk.PhotoImage(file="hourse.png").subsample(5,5)
-cow = tk.PhotoImage(file="hourse.png").subsample(5,5)
+horse = tk.PhotoImage(file="horse.png").subsample(5,5)
+pig = tk.PhotoImage(file="pig.png").subsample(5,5)
+cow = tk.PhotoImage(file="cow.png").subsample(5,5)
 
+l1=tk.Label(window, text="Click the button to hear animal sounds!", font=5)
 b1=tk.Button(window, text="Dog", image=dog, compound="top", width=100, height=100, command="dog")
 b2=tk.Button(window, text="Cat", image=cat, compound="top", width=100, height=100)
 b3=tk.Button(window, text="Chicken", image=chicken, compound="top", width=100, height=100)
-b4=tk.Button(window, text="Hourse", image=hourse, compound="top", width=100, height=100)
+b4=tk.Button(window, text="Horse", image=horse, compound="top", width=100, height=100)
 b5=tk.Button(window, text="Pig", image=pig, compound="top", width=100, height=100)
 b6=tk.Button(window, text="Cow", image=cow, compound="top", width=100, height=100)
 
-b1.place(x=10, y=10)
-b2.place(x=120, y=10)
-b3.place(x=230, y=10)
-b4.place(x=10, y=120)
-b5.place(x=120, y=120)
-b6.place(x=230, y=120)
+l1.place(x=40, y=10)
+b1.place(x=10, y=40)
+b2.place(x=120, y=40)
+b3.place(x=230, y=40)
+b4.place(x=10, y=150)
+b5.place(x=120, y=150)
+b6.place(x=230, y=150)
 #
 
 window.mainloop()
